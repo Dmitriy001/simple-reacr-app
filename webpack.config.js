b19df3330ffa06e41b6c8c024a513bc405015e62
Vladimir Chagin <vladimir.chagin@gmail.com>
2018-04-11 11:14:13 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'html-loader',
+        ],
+      },
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader']
+      }
+    ],
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+  stats: 'errors-only',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    https: false,
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Simple React App',
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.HotModuleReplacementPlugin({}),
+  ],
+  watch: true,
+};
