d27ff3dd06516ceea01dd4ab73fad7d323bb1b33
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-11 19:37:20 +0300

    create checkInputValue func

--- src/js/Timer/index.jsx
+++ src/js/Timer/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-
-
-
 class Timer extends React.Component {
     constructor() {
         super();
@@ -14,32 +11,52 @@ class Timer extends React.Component {
     }
 
     toStop() {
-        this.setState({timerId: clearInterval(this.state.timerId)})
+        this.setState({timerId: clearInterval(this.state.timerId)});
+        document.getElementById('dec').disabled = false;
+        document.getElementById('inc').disabled = false;
     }
 
     increment() {
-        // if (!this.state.delta) {
-        //     alert('Введите целое не нудевое число');
-        //     document.getElementById('dec').disabled = true;
-        //     document.getElementById('inc').disabled = true;
-        //     return false;
-        // }
+        if (!this.checkInputValue(this.state.delta)) {
+            return false;
+        }
         this.setState({value: this.state.value + this.state.delta,
             timerId: setInterval(()=>this.setState({value: this.state.value * this.state.delta}), 1000)
-        })
+        });
+        document.getElementById('dec').disabled = true;
+        document.getElementById('inc').disabled = true;
     }
 
     decrement() {
+        if (!this.checkInputValue(this.state.delta)) {
+            return false;
+        }
         this.setState({value: this.state.value - this.state.delta})
     }
 
     handleChange(event) {
+        document.getElementById('dec').disabled = false;
+        document.getElementById('inc').disabled = false;
         this.setState({delta: parseInt(event.target.value)});
+    }
+
+    checkInputValue(value) {
+        if(isNaN(value) || !value) {
+            document.getElementById('dec').disabled = true;
+            document.getElementById('inc').disabled = true;
+            document.getElementById('text').setAttribute('style', "border-color: red");
+            alert('Введите целое не нудевое число');
+            return false;
+        } else {
+            document.getElementById('dec').disabled = false;
+            document.getElementById('inc').disabled = false;
+            document.getElementById('text').removeAttribute('style');
+            return true
+        }
     }
 
 
     render () {
-        const value = this.state.value;
         return (
             <div>
                 <input id="text" onChange={this.handleChange.bind(this)}/>
