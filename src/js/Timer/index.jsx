8973473cd9c9972174a009541d28ca737c44f21a
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-12 11:19:54 +0300

    last5

--- src/js/Timer/index.jsx
+++ src/js/Timer/index.jsx
@@ -10,26 +10,37 @@ class Timer extends React.Component {
         }
     }
 
-
     toStop() {
         this.setState({timerId: clearInterval(this.state.timerId)});
     }
 
     increment() {
-        if(isNaN(this.state.delta) || this.state.delta == null) {
-            alert('Введите целое не нулевое значение')
+        if (this.checkValue(this.state.delta)) {
+            return;
         }
+
         this.setState({value: this.state.value + this.state.delta,
             timerId: setInterval(()=>this.setState({value: this.state.value * this.state.delta}), 1000)
         });
     }
 
     decrement() {
-        if(isNaN(this.state.delta) || this.state.delta == null) {
-            alert('Введите целое не нулевое значение')
+        if (this.checkValue(this.state.delta)) {
+            return;
         }
-        this.setState({value: this.state.value - this.state.delta})
+
+
+        this.setState({value: this.state.value - this.state.delta,
+            timerId: setInterval(()=>this.setState({value: this.state.value - this.state.delta}), 1000)})
     }
+
+    checkValue(value) {
+        if(isNaN(value) || value == null) {
+            alert('Введите целое не нулевое значение')
+            return true;
+        } else return false;
+    }
+
 
     handleChange(event) {
         this.setState({delta: parseInt(event.target.value)});
@@ -38,12 +49,9 @@ class Timer extends React.Component {
     render () {
         return (
             <div>
-                {isNaN(this.state.delta) ?
-                    <input id="text" onChange={this.handleChange.bind(this)} style={{borderColor: 'red'}}/>
-                    :
-                    <input id="text" onChange={this.handleChange.bind(this)}/>}
-                    <input id="inc" value="increment" onClick={this.increment.bind(this)} type="button" disabled={isNaN(this.state.delta) || this.state.timerId != null}/>
-                    <input id="dec" value="decrement" onClick={this.decrement.bind(this)} type="button" disabled={isNaN(this.state.delta) || this.state.timerId != null}/>
+                <input id="text" onChange={this.handleChange.bind(this)} style={{borderColor: isNaN(this.state.delta) ? 'red' : ''}}/>
+                <input id="inc" value="increment" onClick={this.increment.bind(this)} type="button" disabled={isNaN(this.state.delta) || this.state.timerId != null}/>
+                <input id="dec" value="decrement" onClick={this.decrement.bind(this)} type="button" disabled={isNaN(this.state.delta) || this.state.timerId != null}/>
                 <span className="num">{this.state.value}</span>
                 <input className="stop" value="stop" type="button" onClick={this.toStop.bind(this)}/>
             </div>
