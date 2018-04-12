1c7ce4e952431c2992bb95605d5335a2aa3ddfc1
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-12 22:39:17 +0300

    add handleInputChange func

--- src/js/LoginApp/index.jsx
+++ src/js/LoginApp/index.jsx
@@ -11,28 +11,24 @@ class LoginApp extends React.Component {
     }
 
     handleSubmit(e) {
-        if (!this.state.password || !this.state.email || this.state.password=== true || this.state.email === true){
+        if (!this.state.password || !this.state.email || this.state.password === true || this.state.email === true){
             e.preventDefault();
             alert('Заполните все поля')
         }
 
     }
 
-    handleChangeEmail(event) {
-        this.setState({email: event.target.value});
 
+    handleInputChange(event) {
+        this.setState({[event.target.name]: event.target.value});
     }
 
-    handleChangePass(event) {
-        this.setState({password: event.target.value});
-
-    }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit.bind(this)}>
-                <input type="email" onChange={this.handleChangeEmail.bind(this)} style={{borderColor: !this.state.email ? 'red' : ''}}/>
-                <input type="password" onChange={this.handleChangePass.bind(this)} style={{borderColor: !this.state.password ? 'red' : ''}}/>
+                <input type="email" name="email" onChange={this.handleInputChange.bind(this)} style={{borderColor: !this.state.email ? 'red' : ''}}/>
+                <input type="password" name="password" onChange={this.handleInputChange.bind(this)} style={{borderColor: !this.state.password ? 'red' : ''}}/>
                 <input type="submit" value="Submit"/>
             </form>
         )
