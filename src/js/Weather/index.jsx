24f0e98562b3018b87a4b3dad4c4b67674dd895a
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-15 18:08:40 +0300

    add weather request

--- src/js/Weather/index.jsx
+++ src/js/Weather/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import axios from 'axios'
 
 class Weather extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    Axios() {
+        axios.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=967e07f7bd47f1096a587f111b2ffd28')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
+    render() {
+        this.Axios();
+        return (
+            <div></div>
+        )
+
+
+    }
 }
 
 
