9e08aec2ad1da04cd07892e24112c9d7bc3538c5
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-15 17:19:33 +0300

    install axios

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,14 +4,16 @@ import TimerList from './TimerList';
 import LoginApp from './LoginApp';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Timer from "./Timer";
+import Weather from './Weather'
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
             <Switch>
-                <Route exact path='/' component={LoginApp}/>
+                <Route exact path='/login' component={LoginApp}/>
                 <Route path='/timerlist' component={TimerList}/>
                 <Route path="/timer" component={Timer}/>
+                <Route path="/weather" component={Weather}/>
             </Switch>
         </div>
     </BrowserRouter>,
