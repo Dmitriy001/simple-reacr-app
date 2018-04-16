bb5c7521c753cb59876240acdf06380994a15069
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-16 18:27:26 +0300

    add input

--- src/js/Weather/index.jsx
+++ src/js/Weather/index.jsx
@@ -12,13 +12,18 @@ class Weather extends React.PureComponent{
         this.setState({city: event.target.value});
     };
 
+    inCelsius(val) {
+        return Math.round(val - 274.15);
+    }
+
     handleClick() {
+
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=967e07f7bd47f1096a587f111b2ffd28`)
             .then((response) => {
                 const obj = JSON.parse(response.request.response);
                 this.setState({
                     name: obj.name,
-                    temp: Math.round(obj.main.temp-274.15),
+                    temp: this.inCelsius(obj.main.temp),
                     weather: obj.weather[0].main.toLowerCase()
                 })
             })
@@ -33,10 +38,12 @@ class Weather extends React.PureComponent{
             <div>
                 <input placeholder="City" onChange={this.handleChange.bind(this)}/>
                 <input type="button" value="Show" onClick={this.handleClick.bind(this)}/>
-                <p>{this.state.name}</p>
-                <span>{this.state.temp}&deg;</span>
-                <div className={icon}></div>
-                <p>{this.state.weather}</p>
+                <div className={!this.state.name ? 'hidden' : ''}>
+                    <p>{this.state.name}</p>
+                    <span>{this.state.temp}&deg;</span>
+                    <div className={icon}></div>
+                    <p>{this.state.weather}</p>
+                </div>
             </div>
         )
     }
