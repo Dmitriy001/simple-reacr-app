36787938bde02e94dbf7821b5901a31be3922921
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-16 13:27:00 +0300

    add icon in webpack

--- src/js/Weather/index.jsx
+++ src/js/Weather/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import axios from 'axios'
+import './weather.css'
 
 class Weather extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+
+        }
     }
-    Axios() {
+
+    componentDidMount() {
         axios.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=967e07f7bd47f1096a587f111b2ffd28')
-            .then(function (response) {
-                console.log(response);
+            .then((response) => {
+                console.log(JSON.parse(response.request.response));
+                const obj = JSON.parse(response.request.response);
+                this.setState({
+                    name: obj.name,
+                    temp: Math.round(obj.main.temp-274.15),
+                    weather: obj.weather[0].main.toLowerCase()
+                })
             })
             .catch(function (error) {
                 console.log(error);
@@ -16,14 +27,15 @@ class Weather extends React.Component{
     }
 
     render() {
-        this.Axios();
         return (
-            <div></div>
+            <div>
+                <p>{this.state.name}</p>
+                <p>{this.state.temp}&deg;</p>
+                <p>{this.state.weather}</p>
+                <div className={this.state.weather}></div>
+            </div>
         )
-
-
     }
 }
-
 
 export default Weather
